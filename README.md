5a42449534c6a73c0a7a11e871980a6a723c7d24
athril <patryk.orzechowski@gmail.com>
2022-06-22 23:20:55 -0400

    Creating a basic js call to Pennsieve agent

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # pennsieve-agent-javascript
 Javascript agent for the Pennsieve uploads service
+
+
+# Installation
+1. Install node and node package manager
+
+
+```bash 
+    sudo apt-get install node
+    sudo apt-get install npm
+```
+
+2. Through npm package manager install required libraries. Root may be needed.
+
+```bash
+    npm install async
+    npm install @grpc/grpc-js
+    npm install @grpc/proto-loader
+```
+
+This command will creade a ``modules_node`` folder in the current directory.
+
+3. Start pennsieve server
+```bash
+pennsieve server
+```
+
+Pennsieve server will be running on port 9000.
+
+4. Run the javasript client
+
+```bash
+    node pennsieve.js
+```
--- pennsieve/pennsieve.js
+++ pennsieve/pennsieve.js
@@ -1,6 +1,4 @@
-//var PROTO_PATH = __dirname + '/../../../protos/route_guide.proto';
-//import {userProfile} from './userProfile.js'
-var PROTO_PATH = './protos/agent.proto'
+var PROTO_PATH = __dirname+'/protos/agent.proto'
 var async = require('async');
 var fs = require('fs');
 var parseArgs = require('minimist');
@@ -9,34 +7,6 @@ var _ = require('lodash');
 var grpc = require('@grpc/grpc-js');
 var protoLoader = require('@grpc/proto-loader');
 
-/*
-class Pennsieve {
-  constructor(target){
-    var packageDefinition = protoLoader.loadSync(
-        PROTO_PATH,
-        {keepCase: true,
-         longs: String,
-         enums: String,
-         defaults: true,
-         oneofs: true
-        });
-    var routeguide = grpc.loadPackageDefinition(packageDefinition).routeguide;
-    this.stub = new routeguide.RouteGuide(target, grpc.credentials.createInsecure());
-    this.api = this;
-    //this.manifest = Manifest(this.stub);
-    this.user = UserProfile(this.stub);
-
-
-  }
-
-  getUser() {
-  return this.user.whoami();
-}
-}
-
-
-//create object in a driver function... pennsieve = new Pennsieve(target='localhost:9000');
-*/
 var packageDefinition = protoLoader.loadSync(
     PROTO_PATH,
     {keepCase: true,
@@ -45,9 +15,19 @@ var packageDefinition = protoLoader.loadSync(
      defaults: true,
      oneofs: true
     });
-var routeguide = grpc.loadPackageDefinition(packageDefinition).routeguide;
-var stub = new routeguide.RouteGuide('localhost:9000', grpc.credentials.createInsecure());
+var routeguide = grpc.loadPackageDefinition(packageDefinition);
+console.log(routeguide);
 
-//request=
-//... //import it from GetUserRequest.js
-stub.proto.protos.GetUser(request={})
+
+function main() {
+    var stub = new routeguide.protos.Agent('localhost:9000', grpc.credentials.createInsecure());
+//    console.log(stub)
+
+    stub.getUser({}, function(err, response) {
+        console.log(response);
+        console.log(err);
+    });
+}
+
+
+main()
