e90b07cb83d16b093d1b0e870e663ef8c699112f
athril <patryk.orzechowski@gmail.com>
2022-07-25 13:09:00 -0400

    Removing example from the library

--- pennsieve/pennsieve.js
+++ pennsieve/pennsieve.js
@@ -138,17 +138,4 @@ class Pennsieve {
 };
 
 
-
-logger = function(err, response) {
-    if (err)
-        throw err;
-    console.log(response)
-};
-
-
-p = new Pennsieve()
-p.getUser(callback=logger)
-p.switchProfile('pennsieve-agent', logger)
-p.listManifests(logger);
-//p.useDataset({dataset_id : 'N:dataset:...' })
-//p.createManifest({base_path : 'pennsieve.js'})
+module.exports = Pennsieve
